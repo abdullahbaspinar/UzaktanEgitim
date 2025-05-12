26c05cd94bd8c789bfb7c3cdb1a66b60dc6c7fc0
abaspnr <117168519+abaspnr@users.noreply.github.com>
2025-05-12 13:03:37 +0300

    js

--- Unit1/activites/Unit1/js/unit1_1_2.js
+++ Unit1/activites/Unit1/js/unit1_1_2.js
@@ -25,3 +25,4 @@ const themeToggle = document.getElementById('themeToggle');
       });
       const feedback = document.getElementById('match-feedback');
       feedback.textContent = `${correctCount} doğru eşleşme yapıldı.`;}
+      (function(){if(!window.chatbase||window.chatbase("getState")!=="initialized"){window.chatbase=(...arguments)=>{if(!window.chatbase.q){window.chatbase.q=[]}window.chatbase.q.push(arguments)};window.chatbase=new Proxy(window.chatbase,{get(target,prop){if(prop==="q"){return target.q}return(...args)=>target(prop,...args)}})}const onLoad=function(){const script=document.createElement("script");script.src="https://www.chatbase.co/embed.min.js";script.id="fmGhmZopdiKau9QTCrAFx";script.domain="www.chatbase.co";document.body.appendChild(script)};if(document.readyState==="complete"){onLoad()}else{window.addEventListener("load",onLoad)}})();
--- index.js
+++ index.js
@@ -84,3 +84,5 @@ function sendMail(event) {
   const body = `Ad: ${name}%0AE-posta: ${email}%0AMesaj:%0A${message}`;
   window.location.href = `mailto:aabdullahbaspinarr@gmail.com?subject=${subject}&body=${body}`;
 }
+
+(function(){if(!window.chatbase||window.chatbase("getState")!=="initialized"){window.chatbase=(...arguments)=>{if(!window.chatbase.q){window.chatbase.q=[]}window.chatbase.q.push(arguments)};window.chatbase=new Proxy(window.chatbase,{get(target,prop){if(prop==="q"){return target.q}return(...args)=>target(prop,...args)}})}const onLoad=function(){const script=document.createElement("script");script.src="https://www.chatbase.co/embed.min.js";script.id="fmGhmZopdiKau9QTCrAFx";script.domain="www.chatbase.co";document.body.appendChild(script)};if(document.readyState==="complete"){onLoad()}else{window.addEventListener("load",onLoad)}})();
